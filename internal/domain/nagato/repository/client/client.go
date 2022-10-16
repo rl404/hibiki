a73705f9a8b610b94a519c175fd136cd2408f846
axel <axel.rl.404@gmail.com>
2022-10-16 10:25:36 +0700

    update: newrelic in nagato

--- internal/domain/nagato/repository/client/client.go
+++ internal/domain/nagato/repository/client/client.go
@@ -1,6 +1,12 @@
 package client
 
-import "github.com/rl404/nagato"
+import (
+	"net/http"
+	"time"
+
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/rl404/nagato"
+)
 
 // Client is client for nagato (mal).
 type Client struct {
@@ -9,7 +15,28 @@ type Client struct {
 
 // New to create new nagato (mal) client.
 func New(clientID, clientSecret string) *Client {
+	n := nagato.New(clientID)
+	n.SetHttpClient(&http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &clientIDTransport{
+			clientID: clientID,
+		},
+	})
 	return &Client{
-		client: nagato.New(clientID),
+		client: n,
 	}
+}
+
+type clientIDTransport struct {
+	transport http.RoundTripper
+	clientID  string
+}
+
+func (c *clientIDTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if c.transport == nil {
+		c.transport = http.DefaultTransport
+	}
+	c.transport = newrelic.NewRoundTripper(c.transport)
+	req.Header.Add("X-MAL-CLIENT-ID", c.clientID)
+	return c.transport.RoundTrip(req)
 }
